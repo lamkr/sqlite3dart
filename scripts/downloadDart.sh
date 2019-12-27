fda3a9797a4d92553331be0c52c5b6dd85920367
Luciano Rodrigues <luciano.rodrigues@ailos.coop.br>
2019-12-27 12:18:59 -0200

    adjust varaibles

--- scripts/downloadDart.sh
+++ scripts/downloadDart.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-export DART_SDK_FILENAME=dartsdk-macos-x64-release.zip
-export DART_SDK_CHANNEL=stable
+export DART_SDK_FILENAME="dartsdk-macos-x64-release.zip"
+export DART_SDK_CHANNEL="stable"
 export DART_SDK_VERSION=2.7.0
 export DART_SDK=${HOME}/dart-sdk
 
 ## Download Dart SDK
 mkdir -p ${HOME}/Downloads/
+echo "--connect-timeout 15 --retry 5 -o ${HOME}/Downloads/${DART_SDK_FILENAME} -O https://storage.googleapis.com/dart-archive/channels/${DART_SDK_CHANNEL}/release/${DART_SDK_VERSION}/sdk/${DART_SDK_FILENAME}"
 curl --connect-timeout 15 --retry 5 -o ${HOME}/Downloads/${DART_SDK_FILENAME} -O https://storage.googleapis.com/dart-archive/channels/${DART_SDK_CHANNEL}/release/${DART_SDK_VERSION}/sdk/${DART_SDK_FILENAME}
 unzip ${HOME}/Downloads/${DART_SDK_FILENAME} -d ${HOME}/
 export PATH=$PATH:${DART_SDK}/bin
