703db010fd4a98fa383d08b90cb44ac8f941f053
lamkr <lamkrodrigues@gmail.com>
2019-12-27 15:37:41 -0200

    Update runDart.sh

--- scripts/runDart.sh
+++ scripts/runDart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ## Run Dart tests
-yes | cp -rf ${SQLITE3DART_LIBRARY_PATH}/${SQLITE3DART_LIBRARY_NAME}.* ${TRAVIS_BUILD_DIR}/sqlite3dart/example
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TRAVIS_BUILD_DIR}/sqlite3dart/example
-cd ${TRAVIS_BUILD_DIR}/sqlite3dart/example
+yes | cp -rf ${SQLITE3DART_LIBRARY_PATH}/${SQLITE3DART_LIBRARY_NAME}.* ${TRAVIS_BUILD_DIR}/example
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TRAVIS_BUILD_DIR}/example
+cd ${TRAVIS_BUILD_DIR}/example
 pwd
 dart -v sqlite3dart_test.dart
 #if [ $? -ne 0 ]
@@ -11,3 +11,4 @@ dart -v sqlite3dart_test.dart
 #	echo "# Error running dart tests"
 #	exit 1
 #fi
+exit $?
