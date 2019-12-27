dd8a884d555580de511ce6bc50d5fc572ba2cd20
lamkr <lamkrodrigues@gmail.com>
2019-12-27 16:07:26 -0200

    Update runDart.sh

--- scripts/runDart.sh
+++ scripts/runDart.sh
@@ -4,7 +4,5 @@
 yes | cp -f ${SQLITE3DART_LIBRARY_PATH}/${SQLITE3DART_LIBRARY_NAME}.* ${TRAVIS_BUILD_DIR}/sqlite3dart/example
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TRAVIS_BUILD_DIR}/sqlite3dart/example
 cd ${TRAVIS_BUILD_DIR}/sqlite3dart/example
-pwd
-ls -l
 pub get
 dart -v ./sqlite3dart_test.dart
