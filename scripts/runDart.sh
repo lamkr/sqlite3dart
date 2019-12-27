3e7c0cd4c945198929dd1df47d895870242295e9
lamkr <lamkrodrigues@gmail.com>
2019-12-27 15:26:13 -0200

    add lib vars; adjust rundart.sh

--- scripts/runDart.sh
+++ scripts/runDart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Run Dart tests
-yes | cp -rf ${TRAVIS_BUILD_DIR}/build/libsqlite3dart_extension.so ${TRAVIS_BUILD_DIR}/sqlite3dart/example
+yes | cp -rf ${SQLITE3DART_LIBRARY_PATH}/${SQLITE3DART_LIBRARY_NAME}.* ${TRAVIS_BUILD_DIR}/sqlite3dart/example
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TRAVIS_BUILD_DIR}/sqlite3dart/example
 pushd ${TRAVIS_BUILD_DIR}/sqlite3dart/example
 dart -v sqlite3dart_test.dart
